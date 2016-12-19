5eb16a36d86091c845c629a190c621bb8fb4db35
rene_mbp <harryloveslearning@googlemail.com>
2016-12-19 22:48:41 +0100

    toazure deploy now in root

--- toazure.sh
+++ toazure.sh
@@ -2,6 +2,8 @@
 
 $ngEnvironment=prod
 
+cd deploy
+
 echo === start lightmessageservice
 echo --- deploy lightmessageservice
 
@@ -16,12 +18,24 @@ cd ..
 
 echo === finished lightmessageservice
 
-pwd
+echo
 
-echo ===start lightmessage
+echo === start lightmessage
 echo --- build lightmessage
 
 cd ../UI
 ng build --environment=$ngEnvironment
 cd ../deploy
 
+echo --- deploy lightmessage
+
+cd lightmessage
+git pull
+rm -rf *
+cp -r ../../UI/dist/ .
+git add .
+git commit -am"new deploy"
+git push
+cd ..
+
+echo === finished lightmessage

8d465e5b9a6979bb219ba25ef591e12bd9df46e0
rene_mbp <harryloveslearning@googlemail.com>
2016-12-20 18:21:26 +0100

    deploy 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 gitUsername=$1
 gitPassword=$2
 
+gitCommitMessage="travis-deploy"
+
 ngEnvironment=prod
 deployFolder=deploy
 
@@ -17,14 +19,12 @@ echo --- deploy lightmessageservice
 gitRepo="https://${gitUsername}:${gitPassword}@${gitRepo}"
 
 git clone $gitRepo $deployFolder
-
-pwd
-ls -na
-cp API/ $deployFolder
-git status
-
-pwd
-
+cp -r API/ $deployFolder
+cd $deployFolder
+git add .
+git commit -am"${gitCommitMessage}"
+git push
+cd ..
 
 
 #cd lightmessageservice

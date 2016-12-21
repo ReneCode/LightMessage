4140e1a0cbbc1fdc3fc490ddb8e1a7757728c86a
travis deploy script <abc@abc.com>
2016-12-21 20:33:42 +0100

    try 00

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,24 +1,33 @@
 
-gitCommitMessage="travis-deploy"
+gitCommitMessage="travis"
 
 echo === start azure deploy
 
 gitRepo="https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_SITE}.scm.azurewebsites.net:443/${AZURE_WA_SITE}.git"
 
-git clone $gitRepo 
 
-git config user.email "langner.r@eplan.de"
+git config user.email "myself.r@eath.com"
 git config user.name "travis deploy script"
 
+git clone $gitRepo 
+
+echo a1
 cd ${AZURE_WA_SITE}
 # remove old files inside the repo
 rm -rf *
 
+echo a2
 
 # copy new files
 cp -r ../dist/. .
+
+echo a3
 git add .
+
+echo a4
 git commit -am"${gitCommitMessage}"
+
+echo a5
 git push
 
 cd ..

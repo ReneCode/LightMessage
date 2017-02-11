7eba91f6a6df58bd7c7717595155ea8348247552
travis deploy script <abc@abc.com>
2017-02-11 21:54:30 +0100

    typo on concatMap

--- src/app/light-message.service.ts
+++ src/app/light-message.service.ts
@@ -3,7 +3,7 @@ import { Http, Headers, Response } from '@angular/http'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
-import 'rxjs/add/operator/concatmap'
+import 'rxjs/add/operator/concatMap'
 
 import { environment } from '../environments/environment'
 

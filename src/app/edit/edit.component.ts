a677efd4f13805e19f60d3a0fec28962ab2b7fad
travis deploy script <abc@abc.com>
2017-02-12 10:12:52 +0100

    tidy up

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -31,9 +31,6 @@ export class EditComponent implements OnInit {
 
 
   saveLightMessage() {
-    // this.lightMessageService.save(this.lightMessage, id => {
-    //   this.lightMessage["_id"] = id;
-    // });
     this.lightMessageService.save(this.lightMessage)
       .subscribe(
           id => {
